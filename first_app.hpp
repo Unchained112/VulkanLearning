c0f36f7e745b6a88672bdf6d3cbec90e3714f164
Unchained112 <51079412+Unchained112@users.noreply.github.com>
2022-03-10 23:13:56 +0100

    Update to tutorial 6

---
+++ first_app.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "lve_window.hpp"
+#include "lve_pipeline.hpp"
+#include "lve_swap_chain.hpp"
+#include "lve_device.hpp"
+#include "lve_model.hpp"
+
+namespace lve{
+
+class FirstApp{
+    public:
+    static constexpr int WIDTH = 800;
+    static constexpr int HEIGHT = 600;
+
+    FirstApp();
+    ~FirstApp();
+
+    FirstApp(const FirstApp &) = delete;
+    FirstApp &operator=(const FirstApp &) = delete;
+
+    void run();
+    void sierpinski(
+        std::vector<LveModel::Vertex> &vertices,
+        int depth,
+        glm::vec2 left,
+        glm::vec2 right,
+        glm::vec2 top);
+
+    private:
+    void loadModels();
+    void createPipelineLayout();
+    void createPipeline();
+    void createCommandBuffers();
+    void drawFrame();
+
+    LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+    LveDevice lveDevice{lveWindow};
+    LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+    std::unique_ptr<LvePipeline> lvePipeline;
+    VkPipelineLayout pipelineLayout;
+    std::vector<VkCommandBuffer> commandBuffers;
+    std::unique_ptr<LveModel> lveModel;
+};
+
+
+}
---
+++ lve_model.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <glm/glm.hpp>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include "lve_device.hpp"
+
+namespace lve{
+
+class LveModel{ //tranfer vertex data from cpu to gpu
+    public:
+
+    struct Vertex{
+        glm::vec2 position;
+        static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+        static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+    };
+
+    LveModel(LveDevice &device, const std::vector<Vertex> &vertices);
+    ~LveModel();
+    
+    LveModel(const LveModel &) = delete;
+    LveModel &operator=(const LveModel &) = delete;
+    
+    void bind(VkCommandBuffer commandBuffer);
+    void draw(VkCommandBuffer commandBuffer);
+    
+    private:
+    void createVertexBuffers(const std::vector<Vertex> &vertices);
+
+    LveDevice &lveDevice;
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory; // buffer need manually allocate the memory
+    uint32_t vertexCount;
+};
+
+
+} //namespace lve
